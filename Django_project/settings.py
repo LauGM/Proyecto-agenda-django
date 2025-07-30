6307814b22ea054acab0d2cd8879aed0f6a9c62a
Lau G.M <lmartin@animac.com.ar>
2025-07-30 19:18:16 -0300

    added allowed hosts for render and debug mode

--- Django_project/settings.py
+++ Django_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s=0+l=ucmg!b$(nlwqtw+b6fpl2w!-2$)=@4cn8kji#k(cm4k-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG_VALUE', 'True') == 'True'
 
-ALLOWED_HOSTS = ['https://proyecto-agenda-django.onrender.com', 'localhost', '127.0.0.1']
+if not DEBUG: # Only apply specific host restrictions in production
+    ALLOWED_HOSTS = ['proyecto-agenda-django.onrender.com']
+else:
+    ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
